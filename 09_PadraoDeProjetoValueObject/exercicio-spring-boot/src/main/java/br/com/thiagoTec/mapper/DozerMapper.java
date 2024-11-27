b0cfe4906750075221ad18893c092fc0749c0f4d
Thiago de Jesus <thiago.cordeiros@hotmail.com>
2024-11-27 12:27:28 -0300

    adicionando a configuracao de projeto VO com DozerMapper

---
+++ 09_PadraoDeProjetoValueObject/exercicio-spring-boot/src/main/java/br/com/thiagoTec/mapper/DozerMapper.java
@@ -0,0 +1,30 @@
+package br.com.thiagoTec.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+public class DozerMapper { //dozer é responsavel por converter entidades em VO
+	
+	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	
+	public static <O,D> D parseObject(O origin, Class<D> destination) { //converter obj simples
+		return mapper.map(origin, destination);
+	}
+	
+	
+	public static <O,D> List<D> parseListObjects(List<O> origin, Class<D> destination) { //converter lista
+		
+		List<D> destinationObjects = new ArrayList<>();
+		
+		for (O o : origin) {
+			destinationObjects.add(mapper.map(o, destination));
+		}
+		return destinationObjects;
+	}
+
+	
+	
+}
